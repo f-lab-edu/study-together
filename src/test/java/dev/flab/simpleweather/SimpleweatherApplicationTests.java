06dd6ff1723348338eb732b090726869769d6202
inno <inno@f-lab.kr>
2023-08-21 19:17:19 +0900

    [init]

---
+++ src/test/java/dev/flab/simpleweather/SimpleweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.flab.simpleweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
