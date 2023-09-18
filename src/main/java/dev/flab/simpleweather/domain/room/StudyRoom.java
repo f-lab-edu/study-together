047761991212d7770185fe7065c70aeb604b1528
wooni97 <55667589+wooni97@users.noreply.github.com>
2023-09-18 20:30:04 +0900

    Room 생성 api

--- src/main/java/dev/flab/simpleweather/domain/room/StudyRoom.java
+++ src/main/java/dev/flab/simpleweather/domain/room/StudyRoom.java
@@ -1,12 +1,73 @@
 package dev.flab.simpleweather.domain.room;
 
+import dev.flab.simpleweather.domain.status.Status;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class StudyRoom {
-    int roomId;
-    String roomName;
-    int total;
-    int participantsNum;
-    String createDate;
-    char status;
-    int managerSeqId;
+
+    private int roomId;
+    private String roomName;
+    private int total;
+    private int participantsNum;
+    private String createDate;
+    private Status status;
+    private int managerSeqId;
+
+    private StudyRoom(String roomName, int total, HttpSession httpSession){
+        this.roomName = roomName;
+        this.total = total;
+        setCreateDate();
+        this.managerSeqId = (int)httpSession.getAttribute("seq_id");
+    }
+
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId){
+        this.roomId = roomId;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public int getParticipantsNum() {
+        return participantsNum;
+    }
+
+    private void setCreateDate() {
+        LocalDate now = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String formatedNow = now.format(formatter);
+
+        this.createDate = formatedNow;
+
+    }
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public int getManagerSeqId(HttpSession httpSession) {
+        return managerSeqId;
+    }
+
+
+    public static StudyRoom of(String roomName, int total, HttpSession httpSession){
+        return new StudyRoom(roomName, total, httpSession);
+    }
+
 
 }
---
+++ src/main/java/dev/flab/simpleweather/domain/room/StudyRoomController.java
@@ -0,0 +1,43 @@
+package dev.flab.simpleweather.domain.room;
+
+import dev.flab.simpleweather.system.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.nio.charset.Charset;
+
+@Controller
+public class StudyRoomController {
+
+    private final StudyRoomService studyRoomService;
+
+    @Autowired
+    public StudyRoomController(StudyRoomService studyRoomService) {
+        this.studyRoomService = studyRoomService;
+    }
+
+    @PostMapping("/api/v1/rooms")
+    @ResponseBody
+    public ResponseEntity<Message> create(StudyRoomForm studyRoomForm, HttpSession httpSession){
+
+        StudyRoom studyRoom = StudyRoom.of(studyRoomForm.getRoomName(), studyRoomForm.getTotal(), httpSession);
+
+        studyRoomService.create(studyRoom, httpSession);
+
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        message.setStatus(Message.StatusEnum.OK);
+        message.setData(studyRoom);
+
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+}
