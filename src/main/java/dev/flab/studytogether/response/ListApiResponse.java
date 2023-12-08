681930267e1361e8ac0fe55b6633e6a0e399c68e
wooni97 <jhw970714@gmail.com>
2023-12-08 11:04:39 +0900

    feat: ListApiResponse 메서드 구현

--- src/main/java/dev/flab/studytogether/response/ListApiResponse.java
+++ src/main/java/dev/flab/studytogether/response/ListApiResponse.java
@@ -1,7 +1,24 @@
 package dev.flab.studytogether.response;
 
+import org.springframework.http.HttpStatus;
+
 import java.util.List;
 
 public class ListApiResponse<T> extends CommonResponse {
     List<T> data;
+
+    private ListApiResponse(HttpStatus httpStatus, List<T> data) {
+        this.success = isSuccess(httpStatus.value());
+        this.statusCode = httpStatus.value();
+        this.httpStatus = httpStatus;
+        this.data = data;
+    }
+
+    public static <T> ListApiResponse<T> of(HttpStatus httpStatus, List<T> data) {
+        return new ListApiResponse<>(httpStatus, data);
+    }
+
+    public static <T> ListApiResponse<T> ok(List<T> data) {
+        return of(HttpStatus.OK, data);
+    }
 }
