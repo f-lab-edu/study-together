eb279f308cd8f2529591c42828337265161dc2f3
wooni97 <jhw970714@gmail.com>
2023-12-08 11:23:42 +0900

    refactor: 공통 API 응답 객체 생성에 따른 controller response 수정

--- src/main/java/dev/flab/studytogether/domain/room/controller/StudyRoomApiController.java
+++ src/main/java/dev/flab/studytogether/domain/room/controller/StudyRoomApiController.java
@@ -1,24 +1,23 @@
 package dev.flab.studytogether.domain.room.controller;
 
-
 import dev.flab.studytogether.aop.PostMethodLog;
+import dev.flab.studytogether.domain.room.dto.RoomCreateRequest;
+import dev.flab.studytogether.domain.room.dto.StudyRoomResponse;
 import dev.flab.studytogether.domain.room.entity.StudyRoom;
-import dev.flab.studytogether.domain.room.StudyRoomApiResponse;
 import dev.flab.studytogether.domain.room.service.StudyRoomService;
+import dev.flab.studytogether.response.SingleApiResponse;
 import dev.flab.studytogether.utils.SessionUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
-import java.sql.SQLException;
 
 @RestController
 @Tag(name = "Study Rooom", description = "스터디룸 API")
 public class StudyRoomApiController {
+
     private final StudyRoomService studyRoomService;
 
-    @Autowired
     public StudyRoomApiController(StudyRoomService studyRoomService) {
         this.studyRoomService = studyRoomService;
     }
@@ -26,50 +25,32 @@ public class StudyRoomApiController {
     @PostMapping("/api/v1/rooms")
     @PostMethodLog
     @Operation(summary = "Create room", description = "스터디룸 생성")
-    public StudyRoomApiResponse createRoom(RoomCreateRequestDto requestDto, HttpSession httpSession) throws SQLException {
-        int memberSeqId = SessionUtil.getLoginMemebrSeqId(httpSession);
+    public SingleApiResponse<StudyRoomResponse> createRoom(RoomCreateRequest requestDto, HttpSession httpSession) {
+        int memberSeqId = getMemberSequenceId(httpSession);
         StudyRoom studyRoom = studyRoomService.createRoom(requestDto.getRoomName(), requestDto.getTotal(), memberSeqId);
 
-        return StudyRoomApiResponse.from(studyRoom);
-
+        return SingleApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
     }
-    @GetMapping("/rooms/{roomId}")
+    @GetMapping("/api/v1/rooms/{roomId}")
     @Operation(summary = "Enter Room", description = "스터디룸 입장")
-    public StudyRoomApiResponse enterRoom(@PathVariable int roomId, HttpSession httpSession) throws SQLException {
-        int memberSeqId = SessionUtil.getLoginMemebrSeqId(httpSession);
+    public SingleApiResponse<StudyRoomResponse> enterRoom(@PathVariable int roomId, HttpSession httpSession){
+        int memberSeqId = getMemberSequenceId(httpSession);
         StudyRoom studyRoom = studyRoomService.enterRoom(roomId, memberSeqId);
 
-        return StudyRoomApiResponse.from(studyRoom);
+        return SingleApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
+
     }
-    @DeleteMapping("/rooms/{roomId}")
+    @DeleteMapping("/api/v1/rooms/{roomId}")
     @Operation(summary = "Exit Room", description = "스터디룸 퇴장")
-    public StudyRoomApiResponse exitRoom(@PathVariable int roomId, HttpSession httpSession){
-        int memberSeqId = SessionUtil.getLoginMemebrSeqId(httpSession);
+    public SingleApiResponse<StudyRoomResponse> exitRoom(@PathVariable int roomId, HttpSession httpSession){
+        int memberSeqId = getMemberSequenceId(httpSession);
         StudyRoom studyRoom = studyRoomService.exitRoom(roomId, memberSeqId);
 
-        return StudyRoomApiResponse.from(studyRoom);
+        return SingleApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
     }
 
-
-    /*
-    =========== Dto =============
-    */
-    public class RoomCreateRequestDto{
-        private String roomName;
-        private int total;
-
-        public RoomCreateRequestDto(String roomName, int total) {
-            this.roomName = roomName;
-            this.total = total;
-        }
-
-        public String getRoomName() {
-            return roomName;
-        }
-
-        public int getTotal() {
-            return total;
-        }
+    private int getMemberSequenceId(HttpSession httpSession) {
+        return SessionUtil.getLoginMemberSequenceId(httpSession);
     }
 
 }
