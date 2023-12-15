0126fc65cd32773707d2ca9701d89bcfc5046a66
wooni97 <jhw970714@gmail.com>
2023-12-16 00:13:50 +0900

    refactor: 공통 API 응답 객체 수정에 따른 메서드 반환 타입 수정

--- src/main/java/dev/flab/studytogether/domain/room/controller/StudyRoomApiController.java
+++ src/main/java/dev/flab/studytogether/domain/room/controller/StudyRoomApiController.java
@@ -5,7 +5,7 @@ import dev.flab.studytogether.domain.room.dto.RoomCreateRequest;
 import dev.flab.studytogether.domain.room.dto.StudyRoomResponse;
 import dev.flab.studytogether.domain.room.entity.StudyRoom;
 import dev.flab.studytogether.domain.room.service.StudyRoomService;
-import dev.flab.studytogether.response.SingleApiResponse;
+import dev.flab.studytogether.response.ApiResponse;
 import dev.flab.studytogether.utils.SessionUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,32 +25,32 @@ public class StudyRoomApiController {
     @PostMapping("/api/v1/rooms")
     @PostMethodLog
     @Operation(summary = "Create room", description = "스터디룸 생성")
-    public SingleApiResponse<StudyRoomResponse> createRoom(RoomCreateRequest requestDto, HttpSession httpSession) {
+    public ApiResponse<StudyRoomResponse> createRoom(RoomCreateRequest requestDto, HttpSession httpSession) {
         int memberSeqId = getMemberSequenceId(httpSession);
         StudyRoom studyRoom = studyRoomService.createRoom(requestDto.getRoomName(), requestDto.getTotal(), memberSeqId);
 
-        return SingleApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
+        return ApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
     }
     @GetMapping("/api/v1/rooms/{roomId}")
     @Operation(summary = "Enter Room", description = "스터디룸 입장")
-    public SingleApiResponse<StudyRoomResponse> enterRoom(@PathVariable int roomId, HttpSession httpSession){
+    public ApiResponse<StudyRoomResponse> enterRoom(@PathVariable int roomId, HttpSession httpSession){
         int memberSeqId = getMemberSequenceId(httpSession);
         StudyRoom studyRoom = studyRoomService.enterRoom(roomId, memberSeqId);
 
-        return SingleApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
+        return ApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
 
     }
     @DeleteMapping("/api/v1/rooms/{roomId}")
     @Operation(summary = "Exit Room", description = "스터디룸 퇴장")
-    public SingleApiResponse<StudyRoomResponse> exitRoom(@PathVariable int roomId, HttpSession httpSession){
+    public ApiResponse<StudyRoomResponse> exitRoom(@PathVariable int roomId, HttpSession httpSession){
         int memberSeqId = getMemberSequenceId(httpSession);
         StudyRoom studyRoom = studyRoomService.exitRoom(roomId, memberSeqId);
 
-        return SingleApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
+        return ApiResponse.ok(new StudyRoomResponse(studyRoom.getRoomId(), studyRoom.getRoomName(), studyRoom.getMaxParticipants(), studyRoom.getCurrentParticipants(), studyRoom.getManagerSequenceId()));
     }
 
     private int getMemberSequenceId(HttpSession httpSession) {
-        return SessionUtil.getLoginMemberSequenceId(httpSession);
+        return SessionUtil.getLoginMemebrSeqId(httpSession);
     }
 
 }
