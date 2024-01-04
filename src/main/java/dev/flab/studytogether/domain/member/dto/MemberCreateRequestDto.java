ab20ba7f0f2ea047afee92ae5b090364dcd37948
wooni97 <jhw970714@gmail.com>
2024-01-04 11:50:15 +0900

    refactor: MemberCreateRequestDto 수정
    - 어노테이션을 사용하여 dto 클래스의 코드를 간결하게 표현하고자 수정합니다.

--- src/main/java/dev/flab/studytogether/domain/member/dto/MemberCreateRequestDto.java
+++ src/main/java/dev/flab/studytogether/domain/member/dto/MemberCreateRequestDto.java
@@ -1,32 +1,12 @@
 package dev.flab.studytogether.domain.member.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class MemberCreateRequestDto {
-
     private String id;
-    private String pw;
+    private String password;
     private String nickname;
-
-    public MemberCreateRequestDto(String id, String pw, String nickname) {
-        this.id = id;
-        this.pw = pw;
-        this.nickname = nickname;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getPw() {
-        return pw;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public MemberServiceDto toServiceDto() {
-        return new MemberServiceDto(id, pw, nickname);
-    }
-
 }
