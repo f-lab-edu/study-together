09a4412deb165c7fa913de2defe9d3857fe09eca
wooni97 <jhw970714@gmail.com>
2024-01-18 23:08:46 +0900

    feat: StompWebSocketConfig 클래스 추가

---
+++ src/main/java/dev/flab/studytogether/config/StompWebSocketConfig.java
@@ -0,0 +1,27 @@
+package dev.flab.studytogether.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+        // "/ws/rooms" 는 웹소캣 또는 SocketJS Client가 웹소캣 핸드셰이크 커넥션을 생성할 경로이다.
+        registry.addEndpoint("/stomp-chat")
+                .setAllowedOrigins("http://localhost:8080")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+           config.enableSimpleBroker("/subscribe");
+           config.setApplicationDestinationPrefixes("/publish");
+    }
+}
