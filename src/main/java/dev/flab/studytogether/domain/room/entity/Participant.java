42e863006ebea75c5411b2dbf26a7c995a8151b8
wooni97 <jhw970714@gmail.com>
2024-01-24 12:05:33 +0900

    refactor: Participant 도메인 클래스 수정
    - entryTime 필드 추가
    - 방장 권한 위임시 방장 다음으로 가장 빠르게 입장한 유저를 선택하기 위한 필드 추가

--- src/main/java/dev/flab/studytogether/domain/room/entity/Participant.java
+++ src/main/java/dev/flab/studytogether/domain/room/entity/Participant.java
@@ -1,7 +1,15 @@
 package dev.flab.studytogether.domain.room.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
 public class Participant {
     private int roomId;
     private int seqId;
-
+    // room 입장 시간
+    private LocalDateTime entryTime;
 }
--- src/main/java/dev/flab/studytogether/domain/room/repository/ParticipantRepositoryImpl.java
+++ src/main/java/dev/flab/studytogether/domain/room/repository/ParticipantRepositoryImpl.java
@@ -1,12 +1,16 @@
 package dev.flab.studytogether.domain.room.repository;
 
+import dev.flab.studytogether.domain.room.entity.Participant;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -19,13 +23,14 @@ public class ParticipantRepositoryImpl implements ParticipantRepository {
     }
 
     @Override
-    public void save(int roomId, int seqId) {
+    public void save(int roomId, int seqId, LocalDateTime now) {
         SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
         jdbcInsert.withTableName("PARTICIPANT");
 
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("ROOM_ID", roomId);
         parameters.put("SEQ_ID", seqId);
+        parameters.put("ENTRY_TIME", now);
 
         jdbcInsert.execute(new MapSqlParameterSource(parameters));
     }
@@ -44,7 +49,7 @@ public class ParticipantRepositoryImpl implements ParticipantRepository {
     @Override
     public boolean isMemberExists(int roomId, int memberSequenceId) {
         return jdbcTemplate.queryForObject(
-                "SELECT EXISTS (SELECT 1 FROM PARTICIPANTS WHERE ROOM_ID =? AND SEQ_ID =?",
+                "SELECT EXISTS (SELECT 1 FROM PARTICIPANT WHERE ROOM_ID =? AND SEQ_ID =?)",
                 Boolean.class,
                 roomId,
                 memberSequenceId);
