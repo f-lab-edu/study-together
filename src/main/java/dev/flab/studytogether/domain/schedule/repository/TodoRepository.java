17c1a80f30ba2d216dc7b00b6f43b16c87897669
wooni97 <jhw970714@gmail.com>
2024-04-05 19:45:29 +0900

    refactor: 메소드 파라미터명 변경

--- src/main/java/dev/flab/studytogether/domain/schedule/repository/TodoRepository.java
+++ src/main/java/dev/flab/studytogether/domain/schedule/repository/TodoRepository.java
@@ -11,7 +11,7 @@ public interface TodoRepository {
 
     Todo save(String todoContent, Scheduler scheduler);
     Optional<Todo> find(int schedulerSeq, long todoID);
-    void updateContent(String todoContent, int schdeulrSeq, long todoID);
+    void updateContent(String todoContent, int schedulerSequenceId, long todoID);
     void delete(int schedulerSeq, long todoID);
     void updateCheckStatusToCompleted(int schedulerSeq, long todoID);
     void updateCheckStatusToUncompleted(int schedulerSeq, long todoID);
