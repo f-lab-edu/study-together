548ba4c53e0c7736abad5af919cc16503957e236
wooni97 <jhw970714@gmail.com>
2024-07-18 15:35:41 +0900

    chore: import 추가

--- src/main/java/dev/flab/studytogether/handler/ApiResponseController.java
+++ src/main/java/dev/flab/studytogether/handler/ApiResponseController.java
@@ -5,6 +5,7 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
